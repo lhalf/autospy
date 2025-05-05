1e360853113134207dbf390dccded0fdbcc3143e
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-05-05 10:33:43 +0100

    add support for multiple arguments in trait

--- autospy_macro/src/generate.rs
+++ autospy_macro/src/generate.rs
@@ -11,38 +11,28 @@ pub fn generate(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
 
     for item in &_trait.items {
         if let TraitItem::Fn(method) = item {
-            let function_name = &method.sig.ident;
+            let method_name = &method.sig.ident;
             let arguments = &method.sig.inputs;
 
-            if let Some(FnArg::Typed(pat_type)) = arguments.iter().nth(1) {
-                let argument_name = if let Pat::Ident(ref pat_ident) = *pat_type.pat {
-                    &pat_ident.ident
-                } else {
-                    continue;
-                };
+            let (owned_types, owned_values) = extract_argument_spy_types(arguments);
 
-                let argument_type = *pat_type.ty.clone();
-
-                let (dereference_type, count) = remove_reference(&argument_type);
-
-                let dereferences: proc_macro2::TokenStream = "* "
-                    .repeat(count.saturating_sub(1) as usize)
-                    .parse()
-                    .expect("impossible to fail");
+            if !owned_types.is_empty() {
+                let spy_argument_type = tuple_or_single(&owned_types);
+                let values_to_spy = tuple_or_single(&owned_values);
 
                 spy_fields.push(quote! {
-                    pub #function_name: autospy::SpyFunction<<#dereference_type as ToOwned>::Owned>
+                    pub #method_name: autospy::SpyFunction<#spy_argument_type>
                 });
 
                 spy_trait_impls.push(quote! {
-                    fn #function_name(&self, #argument_name: #argument_type) {
-                        self.#function_name.spy((#dereferences #argument_name).to_owned())
+                    fn #method_name(#arguments) {
+                        self.#method_name.spy(#values_to_spy)
                     }
                 });
             } else {
                 spy_trait_impls.push(quote! {
-                    fn #function_name(&self) {}
-                })
+                    fn #method_name(&self) {}
+                });
             }
         }
     }
@@ -58,6 +48,43 @@ pub fn generate(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
         impl #trait_name for #spy_name {
             #(#spy_trait_impls)*
         }
+    }
+}
+
+fn extract_argument_spy_types(arguments: &syn::punctuated::Punctuated<FnArg, syn::token::Comma>)
+    -> (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>)
+{
+    let mut owned_types = Vec::new();
+    let mut owned_values = Vec::new();
+
+    for argument in arguments {
+        if let FnArg::Typed(pat_type) = argument {
+            let argument_name = match *pat_type.pat {
+                Pat::Ident(ref pat_ident) => &pat_ident.ident,
+                _ => continue,
+            };
+
+            let argument_type = *pat_type.ty.clone();
+            let (dereferenced_type, count) = remove_reference(&argument_type);
+
+            let dereferences: proc_macro2::TokenStream = "* "
+                .repeat(count.saturating_sub(1) as usize)
+                .parse()
+                .expect("impossible to fail");
+
+            owned_types.push(quote! { <#dereferenced_type as ToOwned>::Owned });
+            owned_values.push(quote! { (#dereferences #argument_name).to_owned() });
+        }
+    }
+
+    (owned_types, owned_values)
+}
+
+fn tuple_or_single(inputs: &[proc_macro2::TokenStream]) -> proc_macro2::TokenStream {
+    if inputs.len() == 1 {
+        quote! { #(#inputs)* }
+    } else {
+        quote! { ( #(#inputs),* ) }
     }
 }
 
@@ -96,7 +123,7 @@ mod tests {
                 trait TestTrait {
                     fn function(&self);
                 }
-            }) 
+            })
             .to_string()
         )
     }
@@ -111,7 +138,7 @@ mod tests {
 
                 #[derive(Default, Clone)]
                 struct TestTraitSpy {
-                    pub function: autospy::SpyFunction<<String as ToOwned>::Owned>
+                    pub function: autospy::SpyFunction< <String as ToOwned>::Owned>
                 }
 
                 impl TestTrait for TestTraitSpy {
@@ -140,7 +167,7 @@ mod tests {
 
                 #[derive(Default, Clone)]
                 struct TestTraitSpy {
-                    pub function: autospy::SpyFunction<<str as ToOwned>::Owned>
+                    pub function: autospy::SpyFunction< <str as ToOwned>::Owned>
                 }
 
                 impl TestTrait for TestTraitSpy {
@@ -169,7 +196,7 @@ mod tests {
 
                 #[derive(Default, Clone)]
                 struct TestTraitSpy {
-                    pub function: autospy::SpyFunction<<str as ToOwned>::Owned>
+                    pub function: autospy::SpyFunction< <str as ToOwned>::Owned>
                 }
 
                 impl TestTrait for TestTraitSpy {
@@ -186,5 +213,77 @@ mod tests {
             })
             .to_string()
         )
+    }
+
+    #[test]
+    fn multiple_owned_arguments_non_public_sync_trait() {
+        assert_eq!(quote!{
+            trait TestTrait {
+                fn function(&self, argument1: String, argument2: String);
+            }
+
+            #[derive(Default, Clone)]
+            struct TestTraitSpy {
+                pub function: autospy::SpyFunction<(<String as ToOwned>::Owned, <String as ToOwned>::Owned)>
+            }
+
+            impl TestTrait for TestTraitSpy {
+                fn function(&self, argument1: String, argument2: String) {
+                    self.function.spy(((argument1).to_owned(), (argument2).to_owned()))
+                }
+            }
+         }.to_string(), generate(quote!{
+             trait TestTrait {
+                 fn function(&self, argument1: String, argument2: String);
+             }
+         }).to_string())
+    }
+
+    #[test]
+    fn multiple_borrowed_arguments_non_public_sync_trait() {
+        assert_eq!(quote!{
+            trait TestTrait {
+                fn function(&self, argument1: &str, argument2: &str);
+            }
+
+            #[derive(Default, Clone)]
+            struct TestTraitSpy {
+                pub function: autospy::SpyFunction<(<str as ToOwned>::Owned, <str as ToOwned>::Owned)>
+            }
+
+            impl TestTrait for TestTraitSpy {
+                fn function(&self, argument1: &str, argument2: &str) {
+                    self.function.spy(((argument1).to_owned(), (argument2).to_owned()))
+                }
+            }
+         }.to_string(), generate(quote!{
+             trait TestTrait {
+                 fn function(&self, argument1: &str, argument2: &str);
+             }
+         }).to_string())
+    }
+
+    #[test]
+    fn multiple_multiple_reference_arguments_non_public_sync_trait() {
+        assert_eq!(quote!{
+            trait TestTrait {
+                fn function(&self, argument1: &&&&str, argument2: &&&str);
+            }
+
+            #[derive(Default, Clone)]
+            struct TestTraitSpy {
+                pub function: autospy::SpyFunction<(<str as ToOwned>::Owned, <str as ToOwned>::Owned)>
+            }
+
+            impl TestTrait for TestTraitSpy {
+                fn function(&self, argument1: & & & &str, argument2: & & &str) {
+                    self.function.spy(((*** argument1).to_owned(), (** argument2).to_owned()))
+                }
+            }
+         }.to_string(), generate(quote!{
+             trait TestTrait {
+                 fn function(&self, argument1: &&&&str, argument2: &&&str);
+             }
+         }).to_string())
     }
 }
