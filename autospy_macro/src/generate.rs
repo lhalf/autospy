8bd34521244122ffad1e7b1893307311222263f2
Jacob Araiza <jacob@mlabs.city>
2025-05-14 20:59:31 +0100

    refactoring into smaller functions and renaming things to improve readability

--- autospy_macro/src/generate.rs
+++ autospy_macro/src/generate.rs
@@ -1,39 +1,36 @@
 use quote::{format_ident, quote};
-use syn::{FnArg, ItemTrait, Pat, TraitItem, Type};
+use syn::{FnArg, ItemTrait, Pat, PatType, TraitItem, TraitItemFn, Type};
 
 pub fn generate(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
-    let _trait: ItemTrait = syn::parse2(item.clone()).unwrap();
-    let trait_name = &_trait.ident;
+    let item_trait: ItemTrait = syn::parse2(item.clone()).unwrap();
+    let trait_name = &item_trait.ident;
     let spy_name = format_ident!("{}Spy", trait_name);
 
     let mut spy_fields = Vec::new();
-    let mut spy_trait_impls = Vec::new();
+    let mut spy_function_definitions = Vec::new();
 
-    for item in &_trait.items {
-        if let TraitItem::Fn(method) = item {
-            let method_name = &method.sig.ident;
-            let arguments = &method.sig.inputs;
+    for function in trait_functions(&item_trait) {
+        let function_name = &function.sig.ident;
+        let arguments = &function.sig.inputs;
+        let (owned_types, owned_values) = extract_argument_spy_types(function);
 
-            let (owned_types, owned_values) = extract_argument_spy_types(arguments);
+        if !owned_types.is_empty() {
+            let spy_argument_type = tuple_or_single(&owned_types);
+            let values_to_spy = tuple_or_single(&owned_values);
 
-            if !owned_types.is_empty() {
-                let spy_argument_type = tuple_or_single(&owned_types);
-                let values_to_spy = tuple_or_single(&owned_values);
+            spy_fields.push(quote! {
+                pub #function_name: autospy::SpyFunction<#spy_argument_type>
+            });
 
-                spy_fields.push(quote! {
-                    pub #method_name: autospy::SpyFunction<#spy_argument_type>
-                });
-
-                spy_trait_impls.push(quote! {
-                    fn #method_name(#arguments) {
-                        self.#method_name.spy(#values_to_spy)
-                    }
-                });
-            } else {
-                spy_trait_impls.push(quote! {
-                    fn #method_name(&self) {}
-                });
-            }
+            spy_function_definitions.push(quote! {
+                fn #function_name(#arguments) {
+                    self.#function_name.spy(#values_to_spy)
+                }
+            });
+        } else {
+            spy_function_definitions.push(quote! {
+                fn #function_name(&self) {}
+            });
         }
     }
 
@@ -46,38 +43,49 @@ pub fn generate(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
         }
 
         impl #trait_name for #spy_name {
-            #(#spy_trait_impls)*
+            #(#spy_function_definitions)*
         }
     }
 }
 
-fn extract_argument_spy_types(arguments: &syn::punctuated::Punctuated<FnArg, syn::token::Comma>)
-    -> (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>)
-{
+fn trait_functions(item_trait: &ItemTrait) -> impl Iterator<Item = &TraitItemFn> {
+    item_trait.items.iter().filter_map(|item| match item {
+        TraitItem::Fn(function) => Some(function),
+        _ => None,
+    })
+}
+
+fn extract_argument_spy_types(
+    function: &TraitItemFn,
+) -> (Vec<proc_macro2::TokenStream>, Vec<proc_macro2::TokenStream>) {
     let mut owned_types = Vec::new();
     let mut owned_values = Vec::new();
 
-    for argument in arguments {
-        if let FnArg::Typed(pat_type) = argument {
-            let argument_name = match *pat_type.pat {
-                Pat::Ident(ref pat_ident) => &pat_ident.ident,
-                _ => continue,
-            };
+    for argument in non_self_function_arguments(function) {
+        let argument_name = match *argument.pat {
+            Pat::Ident(ref pat_ident) => &pat_ident.ident,
+            _ => continue,
+        };
 
-            let argument_type = *pat_type.ty.clone();
-            let (dereferenced_type, count) = remove_reference(&argument_type);
+        let (dereferenced_type, count) = remove_references(&argument.ty);
 
-            let dereferences: proc_macro2::TokenStream = "* "
-                .repeat(count.saturating_sub(1) as usize)
-                .parse()
-                .expect("impossible to fail");
+        let dereferences: proc_macro2::TokenStream = "* "
+            .repeat(count.saturating_sub(1) as usize)
+            .parse()
+            .expect("impossible to fail");
 
-            owned_types.push(quote! { <#dereferenced_type as ToOwned>::Owned });
-            owned_values.push(quote! { (#dereferences #argument_name).to_owned() });
-        }
+        owned_types.push(quote! { <#dereferenced_type as ToOwned>::Owned });
+        owned_values.push(quote! { (#dereferences #argument_name).to_owned() });
     }
 
     (owned_types, owned_values)
+}
+
+fn non_self_function_arguments(function: &TraitItemFn) -> impl Iterator<Item = &PatType> {
+    function.sig.inputs.iter().filter_map(|input| match input {
+        FnArg::Typed(argument) => Some(argument),
+        _ => None,
+    })
 }
 
 fn tuple_or_single(inputs: &[proc_macro2::TokenStream]) -> proc_macro2::TokenStream {
@@ -88,10 +96,10 @@ fn tuple_or_single(inputs: &[proc_macro2::TokenStream]) -> proc_macro2::TokenStr
     }
 }
 
-fn remove_reference(argument_type: &Type) -> (Type, u8) {
+fn remove_references(argument_type: &Type) -> (Type, u8) {
     match argument_type {
         Type::Reference(referenced_argument) => {
-            let (dereferenced_argument, count) = remove_reference(&referenced_argument.elem);
+            let (dereferenced_argument, count) = remove_references(&referenced_argument.elem);
             (dereferenced_argument, count + 1)
         }
         argument_type => (argument_type.clone(), 0),
