46adb20f025ca4dcd0c2cf1b67f29537e6a4a539
Jacob Araiza <jacob@mlabs.city>
2025-05-14 22:35:44 +0100

    Integration test for return types in autospies

--- tests/integration.rs
+++ tests/integration.rs
@@ -122,3 +122,24 @@ fn multiple_multiple_reference_argument_sync_trait() {
         spy.function.arguments.take_all()
     )
 }
+
+#[test]
+fn single_function_trait_with_no_arguments_returning_u8() {
+    #[autospy]
+    trait TestTrait {
+        fn function(&self) -> u8;
+    }
+
+    fn use_test_trait<T: TestTrait>(trait_object: T) -> u8 {
+        trait_object.function()
+    }
+
+    let spy = TestTraitSpy::default();
+    spy.function.returns.push_back(0);
+    spy.function.returns.push_back(1);
+
+    assert_eq!(use_test_trait(spy.clone()), 0);
+    assert_eq!(use_test_trait(spy.clone()), 1);
+
+    assert_eq!(vec![(), ()], spy.function.arguments.take_all())
+}
