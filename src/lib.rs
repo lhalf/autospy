d20b896ae7ce64228a9013ca2fd46f4adc4c3e19
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-15 18:03:00 +0100

    add basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// vim: tw=80
+//! A test spy object library.
+//!
+//! autospy is a macro to create spy versions of almost any trait.
+//! They can be used in unit tests as a stand-in for the real object.
+//!
+//! # Usage
+//!
+//! To use autospy simply use the [`#[autospy]`](attr.autospy.html).
+
 mod spy_function;
 
 pub use spy_function::SpyFunction;
