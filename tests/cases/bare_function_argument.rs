1f72a1a35c0d225d1e105777c3d8106f9a19ef8e
Jacob Araiza <jacob@mlabs.city>
2025-05-15 23:26:25 +0100

    more test cases to cover more exotic function arguments

---
+++ tests/cases/bare_function_argument.rs
@@ -0,0 +1,25 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self, function: fn() -> u8);
+}
+
+fn use_test_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function(eight);
+}
+
+fn eight() -> u8 {
+    8
+}
+
+#[test]
+fn borrowed_argument_coverted_to_owned() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.push_back(());
+
+    use_test_trait(spy.clone());
+
+    assert_eq!(
+        eight as *const (),
+        spy.function.arguments.take_all()[0] as *const ()
+    )
+}
--- tests/cases/mod.rs
+++ tests/cases/mod.rs
@@ -1,3 +1,5 @@
+mod array_argument;
+mod bare_function_argument;
 mod multiple_methods_with_no_arguments;
 mod single_borrowed_argument;
 mod single_owned_argument;
