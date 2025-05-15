1c71f5ce72b931cc29ac919285f1b04853705bdd
Jacob Araiza <jacob@mlabs.city>
2025-05-15 23:26:25 +0100

    more test cases to cover more exotic function arguments

---
+++ tests/cases/array_argument.rs
@@ -0,0 +1,18 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self, function: [u8; 1]);
+}
+
+fn use_test_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function([0; 1]);
+}
+
+#[test]
+fn borrowed_argument_coverted_to_owned() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.push_back(());
+
+    use_test_trait(spy.clone());
+
+    assert_eq!(vec![[0; 1]], spy.function.arguments.take_all())
+}
