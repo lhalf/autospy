345dfcd4b0f415edb4259ce264a56dbeb7ff2e02
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-16 11:26:17 +0100

    add acceptance test for return order

---
+++ tests/cases/owned_returns.rs
@@ -0,0 +1,18 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self) -> String;
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) -> String {
+    trait_object.function()
+}
+
+#[test]
+fn returns_values_in_order() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.push_back("first_return".to_string());
+    spy.function.returns.push_back("second_return".to_string());
+
+    assert_eq!("first_return", use_trait(spy.clone()));
+    assert_eq!("second_return", use_trait(spy.clone()));
+}
