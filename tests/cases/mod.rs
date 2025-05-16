dcf2233f7fe31bdf17d6d7929775a3b3230dcd22
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-16 11:06:00 +0100

    add acceptance test for trait arguments

--- tests/cases/mod.rs
+++ tests/cases/mod.rs
@@ -4,3 +4,4 @@ mod function_arguments;
 mod ignore_attribute_arguments;
 mod multiple_trait_methods;
 mod owned_arguments;
+mod trait_arguments;
---
+++ tests/cases/trait_arguments.rs
@@ -0,0 +1,18 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self, argument: impl ToString + 'static);
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function("hello");
+}
+
+#[test]
+fn trait_argument_captured_in_box() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.push_back(());
+
+    use_trait(spy.clone());
+
+    assert_eq!("hello", spy.function.arguments.take_all()[0].to_string())
+}
