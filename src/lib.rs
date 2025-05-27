5ec8bdcfecd668d68a75f046098f071df5fede48
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-05-27 15:22:08 +0100

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@
 //!
 //! # Usage
 //!
-//! To use autospy simply use the [`#[autospy]`](attr.autospy.html).
+//! To use autospy simply attribute your trait using [`#[autospy]`](attr.autospy.html).
+//!
+//! **Note:** If you're using the spy object for tests it is recommended to use `#[cfg_attr(test, autospy)]`.
 //! ```rust
 //! use autospy::autospy;
 //!
@@ -24,6 +26,80 @@
 //!
 //! assert_eq!(20, call_with_ten(&spy.clone()));
 //! assert_eq!(vec![10], spy.foo.arguments.take_all());
+//! ```
+//!
+//! ## References
+//!
+//! `autospy` will automatically convert reference arguments into owned types.
+//!
+//! ```rust
+//! use autospy::autospy;
+//!
+//! #[autospy]
+//! trait MyTrait {
+//!     fn foo(&self, argument: &str);
+//! }
+//!
+//! fn use_trait(x: &dyn MyTrait) {
+//!     x.foo("hello!")
+//! }
+//!
+//! let spy = MyTraitSpy::default();
+//! spy.foo.returns.push_back(());
+//!
+//! use_trait(&spy.clone());
+//!
+//! assert_eq!(vec!["hello!"], spy.foo.arguments.take_all());
+//! ```
+//!
+//! ## Associated types
+//!
+//! An `#[autospy(TYPE)]` attribute can be applied to associated types to tell `autospy` how to capture them.
+//!
+//! ```rust
+//! use autospy::autospy;
+//!
+//! #[autospy]
+//! trait MyTrait {
+//!     #[autospy(String)]
+//!     type Item;
+//!     fn foo(&self, argument: Self::Item);
+//! }
+//!
+//! fn use_trait(x: &dyn MyTrait<Item=String>) {
+//!     x.foo("hello!".to_string())
+//! }
+//!
+//! let spy = MyTraitSpy::default();
+//! spy.foo.returns.push_back(());
+//!
+//! use_trait(&spy.clone());
+//!
+//! assert_eq!(vec!["hello!"], spy.foo.arguments.take_all());
+//! ```
+//!
+//! ## Ignore arguments
+//!
+//! Arguments can be ignored using `#[autospy(ignore)]` if you do not wish to capture them in the spy.
+//!
+//! ```rust
+//! use autospy::autospy;
+//!
+//! #[autospy]
+//! trait MyTrait {
+//!     fn foo(&self, #[autospy(ignore)] ignored: &str, argument: &str);
+//! }
+//!
+//! fn use_trait(x: &dyn MyTrait) {
+//!     x.foo("ignored!", "capture me!")
+//! }
+//!
+//! let spy = MyTraitSpy::default();
+//! spy.foo.returns.push_back(());
+//!
+//! use_trait(&spy.clone());
+//!
+//! assert_eq!(vec!["capture me!"], spy.foo.arguments.take_all());
 //! ```
 mod spy_function;
 
