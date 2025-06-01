281bc3106f62913c178da64877b5d8bdd7e54e84
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-06-01 22:37:45 +0100

    refactor no attribute on associated const to use const-default crate

--- tests/cases/associated_const_attribute.rs
+++ tests/cases/associated_const_attribute.rs
@@ -4,9 +4,12 @@ use autospy::autospy;
 trait TestTrait {
     #[autospy(20)]
     const VALUE: u64;
+    const DEFAULT_VALUE: bool;
 }
 
 #[test]
-fn trait_with_associated_const_uses_attribute_value() {
-    assert_eq!(20, TestTraitSpy::VALUE)
+#[allow(clippy::assertions_on_constants)]
+fn trait_with_associated_const_uses_attribute_value_or_default_if_not_specified() {
+    assert_eq!(20, TestTraitSpy::VALUE);
+    assert!(!TestTraitSpy::DEFAULT_VALUE);
 }
