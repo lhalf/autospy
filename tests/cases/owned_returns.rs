48329306f9c46a395f8d0a30afd78163f94f0243
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-06-10 20:30:48 +0100

    update owned_returns test

--- tests/cases/owned_returns.rs
+++ tests/cases/owned_returns.rs
@@ -12,8 +12,10 @@ fn use_trait<T: MyTrait>(trait_object: T) -> String {
 #[test]
 fn returns_values_in_order() {
     let spy = MyTraitSpy::default();
-    spy.function.returns.push_back("first_return".to_string());
-    spy.function.returns.push_back("second_return".to_string());
+    spy.function
+        .returns
+        .push_back("first_return".to_string())
+        .push_back("second_return".to_string());
 
     assert_eq!("first_return", use_trait(spy.clone()));
     assert_eq!("second_return", use_trait(spy));
