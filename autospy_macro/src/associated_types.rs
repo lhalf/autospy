28a07a830eeec9f882b2879124c81c1697fb8dca
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-06-17 15:17:10 +0100

    formatting

--- autospy_macro/src/associated_types.rs
+++ autospy_macro/src/associated_types.rs
@@ -1,5 +1,5 @@
-use std::collections::BTreeMap;
 use crate::attribute;
+use std::collections::BTreeMap;
 use syn::{Ident, ItemTrait, TraitItem, TraitItemType, TypePath};
 
 pub type AssociatedSpyTypes = BTreeMap<Ident, TypePath>;
