8f9033b0521421b4669addc1512121533fa2feef
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-09-15 22:28:44 +0100

    supports multiple supertraits

---
+++ tests/multiple_supertraits.rs
@@ -0,0 +1,32 @@
+#[autospy::autospy]
+trait MyTrait: Supertrait + Ultratrait {
+    fn function(&self);
+    #[autospy(supertrait = "Supertrait")]
+    fn super_function(&self);
+    #[autospy(supertrait = "Ultratrait")]
+    fn ultra_function(&self);
+}
+
+trait Supertrait {
+    fn super_function(&self);
+}
+
+trait Ultratrait {
+    fn ultra_function(&self);
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function();
+    trait_object.super_function();
+    trait_object.ultra_function();
+}
+
+#[test]
+fn multiple_supertraits_are_supported() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.set([()]);
+    spy.super_function.returns.set([()]);
+    spy.ultra_function.returns.set([()]);
+
+    use_trait(spy);
+}
---
+++ tests/supertrait.rs
@@ -0,0 +1,24 @@
+#[autospy::autospy]
+trait MyTrait: Supertrait {
+    fn function(&self);
+    #[autospy(supertrait = "Supertrait")]
+    fn super_function(&self);
+}
+
+trait Supertrait {
+    fn super_function(&self);
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function();
+    trait_object.super_function();
+}
+
+#[test]
+fn supertraits_are_supported() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.set([()]);
+    spy.super_function.returns.set([()]);
+
+    use_trait(spy);
+}
