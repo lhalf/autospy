67d022b483bb882dd2082626dbe9279903c2453a
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-01 18:19:17 +0000

    supports generic trait functions with lifetimes

---
+++ tests/generic_trait_function_with_lifetime.rs
@@ -0,0 +1,19 @@
+#[allow(clippy::needless_lifetimes)]
+#[autospy::autospy]
+trait MyTrait {
+    fn function<'a>(&self, value: &'a str) -> u8;
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) -> u8 {
+    trait_object.function("hello")
+}
+
+#[test]
+fn trait_functions_can_have_lifetimes() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.set([10]);
+
+    assert_eq!(10, use_trait(spy.clone()));
+
+    assert_eq!(["hello"], spy.function.arguments)
+}
