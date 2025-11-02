1ab9c0f93636a5aa62df1b1533a4582eec4fa267
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-02 17:00:29 +0000

    supports non generic traits with specializing functions

---
+++ tests/specializing_function.rs
@@ -0,0 +1,20 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self, argument: u32)
+    where
+        Self: Sized;
+}
+
+fn use_trait<T: MyTrait>(trait_object: T) {
+    trait_object.function(10)
+}
+
+#[test]
+fn supports_specializing_function_in_trait() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.set([()]);
+
+    use_trait(spy.clone());
+
+    assert_eq!([10], spy.function.arguments);
+}
