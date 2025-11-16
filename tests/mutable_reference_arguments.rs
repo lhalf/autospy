a191ade3a2f1f84481055ff22e84c34c6d4114de
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-16 16:16:31 +0000

    add test for mutable reference arguments

---
+++ tests/mutable_reference_arguments.rs
@@ -0,0 +1,18 @@
+#[autospy::autospy]
+trait MyTrait {
+    fn function(&self, argument: &mut u8);
+}
+
+fn use_trait<T: MyTrait>(trait_object: &T) {
+    trait_object.function(&mut 10)
+}
+
+#[test]
+fn supports_mutable_reference_arguments() {
+    let spy = MyTraitSpy::default();
+    spy.function.returns.set([()]);
+
+    use_trait(&spy);
+
+    assert_eq!([10], spy.function.arguments);
+}
