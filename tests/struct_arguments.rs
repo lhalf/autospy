e3bd3c6cf66fedd60842fcdccd924e3d34930ee8
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-16 16:38:23 +0000

    add test for non debug struct arguments

--- tests/struct_arguments.rs
+++ tests/struct_arguments.rs
@@ -28,3 +28,28 @@ fn non_clone_struct_function_argument_captured() {
         spy.function.arguments
     )
 }
+
+struct NonDebugStruct {
+    value: String,
+}
+
+#[autospy::autospy]
+trait MyTrait2 {
+    fn function(&self, argument: NonDebugStruct);
+}
+
+fn use_trait2<T: MyTrait2>(trait_object: T) {
+    trait_object.function(NonDebugStruct {
+        value: "hello".to_string(),
+    });
+}
+
+#[test]
+fn non_debug_struct_function_argument_captured() {
+    let spy = MyTrait2Spy::default();
+    spy.function.returns.set([()]);
+
+    use_trait2(spy.clone());
+
+    assert_eq!("hello", spy.function.arguments.get()[0].value)
+}
